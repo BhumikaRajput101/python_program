334c104e259979cc6186d4ffb6eaa998c8541fee
BhumikaRajput101 <rajputbhumi101@gmail.com>
2026-02-08 14:13:08 +0530

    Python_Pratice

---
+++ largest_among_three_numbers.py
@@ -0,0 +1,15 @@
+# largst among three number
+a=int(input("enter 1 number "))
+
+b=int(input("enter 2 numeber"))
+c=int(input("enter 3 number "))
+if a>b:
+    if a>c:
+        print(a , " is largest amoung all")
+    else : 
+        print(c, " is largest amoung all ")
+elif b>c:
+    print(b,"is the largest amoung all ")
+
+else : 
+     print(c, " is largest amoung all ")
