1fa2f02eb8108ab92231eabed779c87a26911db9
BhumikaRajput101 <rajputbhumi101@gmail.com>
2026-02-11 17:43:29 +0530

    odd_number.py

--- odd_number.py
+++ odd_number.py
@@ -1,7 +1,10 @@
+## To Calculate The Entered Number Is Even Or Odd
+
 def even(a):
     if a%2==0:
         print ('Entered number is even') 
     else:
         print ('entered number is odd')
 a=int(input("enter number a number:"))
+
 even (a)
