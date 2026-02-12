dded8e65a0f5d7ff450443f266ca768ea43d005b
BhumikaRajput101 <rajputbhumi101@gmail.com>
2026-02-12 18:24:30 +0530

    Palindrome_number

---
+++ palindrome_number.py
@@ -0,0 +1,9 @@
+## To check wheather the value is palindrome or not
+
+a=input ("Enter a value for palindrome check:")
+count=a[::-1]
+if (a==count):
+        print("Yes,The value is palindrome")
+else :
+        print ("No,The value is not palindrome")
+    
