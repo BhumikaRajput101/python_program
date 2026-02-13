ad25bf5836f5287b625d7e5f3f08cc2b882bb84e
BhumikaRajput101 <rajputbhumi101@gmail.com>
2026-02-13 20:10:57 +0530

    Use of lambda function

--- lambda.py
+++ lambda.py
@@ -1,7 +1,8 @@
-#lamba function 
+#lambda function 
 
 addition = lambda a,b : a+b
 
 a=int(input('Enter 1st number:'))
 b=int(input('Enter 2nd number:'))
+
 print('The sum of the Following number is ',addition(a,b))
