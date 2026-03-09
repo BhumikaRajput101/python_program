6ff5ae52d4bbe7116725b9b8071d73766c7913ed
BhumikaRajput101 <rajputbhumi101@gmail.com>
2026-03-09 18:25:21 +0530

    Custom Exception

---
+++ OOPS/Custom Exception.py
@@ -0,0 +1,14 @@
+#magic method 
+class dobexception(Exception):
+    pass
+
+year = int(input ("Enter your birth year"))
+age=2026-year
+try :
+
+ if age <= 30 and age>= 20:
+    print("This is VALID so you can apply for the exam ")
+ else: 
+    raise dobexception()
+except dobexception :
+    print("sorry your age should be  between 20 and 30")
